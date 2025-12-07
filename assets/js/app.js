75cd2bb7ace444f542b13d3d144ec0b8db842bf0
labibj <ljamal2011@gmail.com>
2025-12-07 18:10:25 +0500

    navtabs, offcanvas, modal, popovers, tooltip, progressbar, spinners

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+// Popover
+
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+
+// ToolTip
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
